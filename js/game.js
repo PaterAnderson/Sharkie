16471840928a180cff69ef744874063af1f1d10a
PaterAnderson <DariusMue@web.de>
2024-11-12 13:50:26 +0100

    fixed touch context menu

--- js/game.js
+++ js/game.js
@@ -3,10 +3,14 @@ let world;
 let keyboard = new Keyboard();
 let isPaused = false; 
 
+window.addEventListener('touchstart', function(event) {
+    if (event.touches.length > 0) {
+        event.preventDefault(); 
+    }
+}, { passive: false });
+
 window.oncontextmenu = function(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    return false;
+    return true; 
 };
 
 function init() {
